e5d849f33fa15e47ebd1fc97f7db5a3d2341a9af
CyberTekFA <faiz.m.amiree@gmail.com>
2024-02-22 19:02:14 -0500

    add springboot concepts

--- spring-06-springboot-demo/src/main/java/com/hypertek/HypertekApplication.java
+++ spring-06-springboot-demo/src/main/java/com/hypertek/HypertekApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HypertekApplication {
-
-
-
     public static void main(String[] args) {
         var comment = new Comment();
         comment.setAuthor("John");
---
+++ spring-07-springbootMVC/src/main/java/com/hypertek/Spring07SpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.hypertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07SpringbootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring07SpringbootMvcApplication.class, args);
+    }
+
+}
