c37a2222195927a42dd4210742e289ddf053c9ed
CyberTekFA <faiz.m.amiree@gmail.com>
2024-02-24 11:59:44 -0500

    add spring mvc model concepts

--- spring-08-spring-Model/src/main/java/com/hypertek/controller/StudentController.java
+++ spring-08-spring-Model/src/main/java/com/hypertek/controller/StudentController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/student")
     public String getHomePage(Model module){
         module.addAttribute("name","HyperTek");
         module.addAttribute("course","Spring MVC");
+        int random = (int) Math.floor(new Random().nextInt());
+        List<Integer> ran = new ArrayList<>();
+        ran.add(random);
+        module.addAttribute("id", ran);
         return "students/welcome";
     }
 }
