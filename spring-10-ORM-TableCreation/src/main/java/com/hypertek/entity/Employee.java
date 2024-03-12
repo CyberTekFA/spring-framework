207212936b190ab200bc84eb3406cbca6e5cc70b
CyberTekFA <faiz.m.amiree@gmail.com>
2024-03-11 20:43:56 -0400

    add ORM concept

---
+++ spring-10-ORM-TableCreation/src/main/java/com/hypertek/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.hypertek.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+    private String firstname;
+    private String lastname;
+}
---
+++ spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+insert into Employees(id,name)
+values(1,'Mike Smith');
