f2bb13b55e506b803f61a250b4ee8de909bad6d9
CyberTekFA <faiz.m.amiree@gmail.com>
2024-03-25 18:14:36 -0400

    ORM Mapping CINEMA LAB

--- spring-13-ORM-CinemaLab/src/main/java/com/hypertek/entity/Cinema.java
+++ spring-13-ORM-CinemaLab/src/main/java/com/hypertek/entity/Cinema.java
@@ -1,6 +1,8 @@
 package com.hypertek.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Cinema extends BaseEntity{
+
+    private String name;
+    private String sponsoredName;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
 }
--- spring-13-ORM-CinemaLab/src/main/java/com/hypertek/entity/Location.java
+++ spring-13-ORM-CinemaLab/src/main/java/com/hypertek/entity/Location.java
@@ -10,4 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Location extends BaseEntity{
+    private String name;
+
 }
